3e273a0ecaa8c8893eebca42b913f80d1b887b4c
kema-ray <rachelkemuma99@gmail.com>
2022-12-19 17:25:35 +0300

    add functions to helper file

--- helper.c
+++ helper.c
@@ -49,3 +49,77 @@ int str_len(char *str)
 		;
 	return (i);
 }
+/**
+ * _strncmp -  compares two strings to find out if they are exactly the same
+ * @name: input supplied by the user
+ * @var: variable to compare against
+ * @len: length of the input
+ *
+ *Return: 1 if strings are equal, -1 if they are not
+*/
+int _strncmp(char *name, char *var, unsigned int len)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < len)
+	{
+		if (name[i] != var[i])
+			return (-1);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * _strcmp - compares two strings
+ * @s1: First string
+ * @s2: Second string
+ * Return: 0 if strings match. -1 Otherwise.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	if (str_len(s1) != str_len(s2))
+		return (-1);
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (-1);
+	}
+	return (0);
+}
+
+/**
+ * print_str - Prints a string character by character.
+ * @str: String to be printed. If the string is NULL it will print (null)
+ * @new_line: If integer is 0 a new line will be printed. Otherwise a new line
+ * will not be printed.
+ */
+void print_str(char *str, int new_line)
+{
+	int i;
+
+	if (str == NULL)
+		str = "(null)";
+	for (i = 0; str[i] != '\0'; i++)
+		write(STDOUT_FILENO, &str[i], 1);
+	if (new_line == 0)
+		write(STDOUT_FILENO, "\n", 1);
+}
+
+char *_strdup(char *src)
+{
+	int i;
+	int len;
+	char *dest;
+
+	len = str_len(src);
+	dest = malloc(sizeof(char) * (len + 1));
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
