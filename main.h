a9e9fb179e3b6d864d19f9b1b5a8c43ea7cb488e
Epicsociety <measkswho@gmail.com>
2022-12-21 10:47:45 +0300

    Shell running. ->> Exit not working, betty not checkedgit add .

--- main.h
+++ main.h
@@ -15,6 +15,23 @@ extern char **environ;
 #define PROMPT "$ " /* error strings */
 #define BUFFER 1024
 
+#define ERR_MALLOC "Unable to malloc space\n"
+#define ERR_FORK "Unable to fork and create child process\n"
+#define ERR_PATH "No such file or directory\n"
+
+/**
+ * struct built_s - linked list of builtins
+ * @name: name of builtin
+ * @p: pointer to function
+ *
+ * Description: struct for builtin functions
+*/
+typedef struct built_s{
+	char *name;
+	int (*p)(void);
+} built_s;
+
+
 char *_getline(FILE *fp);
 void prompt(int fd, struct stat buf);
 char **tokenizer(char *str);
@@ -29,20 +46,30 @@ char *path_finder(char *command);
 char **tokenize_path(int index, char *str);
 char *build_path(char *directory, char *command);
 char *search_directories(char **path_tokens, char *command);
+int shell_env(void);
+int shell_exit(void);
+int builtin_execute(char **tokens);
+int shell_num_builtins(built_s builtin[]);
+char *_getenv(const char *name);
+char **copy_env(char **environ_copy, unsigned int environ_length);
+char *_which(char *command, char *fullpath, char *path);
+int child(char *fullpath, char **tokens);
+void errors(int error);
+void free_all(char **tokens, char *path, char *line, char *fullpath, int flag);
+void free_dp(char **array, unsigned int length);
 
-/*Utility functions (string tools)*/
-int _strncmp(char *name, char *var, unsigned int len);
-
-/*Other essential functions */
 
 /* builtin functions */
 void cd_p(char *);
 
-/*Holberton library functions*/
+/*Helper functions (string tools)*/
 int _strcmp(char *, char *);
+int _strlen(char *str);
 char *_strdup(char *);
 void print_str(char *, int);
 int print_number(int);
 int _write_char(char);
+int _strncmp(char *name, char *var, unsigned int len);
+char *_strcpy(char *dest, char *src);
 
 #endif
--- simpleshell.c
+++ simpleshell.c
@@ -7,9 +7,10 @@
 */
 int main(void)
 {
-	char *line;
+	char *line, *path, *fullpath;
 	char **tokens;
 	struct stat buf;
+	int builtin_status, flag, child_status;
 
 	while (1) /* True, infinite loop */
 	{
@@ -23,6 +24,27 @@ int main(void)
 		tokens = tokenizer(line);
 		if (tokens[0] == NULL)
 			continue;
+		builtin_status = builtin_execute(tokens);
+		if (builtin_status == 0 || builtin_status == -1)
+		{
+			free(tokens);
+			free(line);
+		}
+		if (builtin_status == 0)
+			continue;
+		if (builtin_status == -1)
+			_exit(EXIT_SUCCESS);
+		flag = 0; /* 0 if full_path is not malloc'd */
+		path = _getenv("PATH");
+		fullpath = _which(tokens[0], fullpath, path);
+		if (fullpath == NULL)
+			fullpath = tokens[0];
+		else
+			flag = 1; /* if fullpath was malloc'd, flag to free */
+		child_status = child(fullpath, tokens);
+		if (child_status == -1)
+			errors(2);
+		free_all(tokens, path, line, fullpath, flag);
 	}
 
 	return (0);
