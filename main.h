af0efe875f28a2d454ab2f4b4b6ef7ea89fac487
Epicsociety <measkswho@gmail.com>
2022-12-24 00:29:20 +0300

    Several changes. New files added, some files deleted

--- main.h
+++ main.h
@@ -1,171 +1,81 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/* HEADER FILES */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <stdarg.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <limits.h>
-#include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
 #include <sys/wait.h>
+#include <string.h>
+#include <limits.h>
+#include <dirent.h>
+#include <stdarg.h>
 #include <errno.h>
 
-/* for read/write buffers */
-#define READ_BUF_SIZE 1024
-#define WRITE_BUF_SIZE 1024
-#define BUF_FLUSH -1
-#define HIST_FILE ".history"
+/*-- ENVIRONMENT --*/
 
 extern char **environ;
 
-#define PROMPT "$ " /* error strings */
-#define BUFFER 1024
-
-#define ERR_MALLOC "Unable to malloc space\n"
-#define ERR_FORK "Unable to fork and create child process\n"
-#define ERR_PATH "No such file or directory\n"
+/* STRUCTS */
 
 /**
- * struct built_s - linked list of builtins
- * @name: name of builtin
- * @p: pointer to function
- *
- * Description: struct for builtin functions
-*/
-typedef struct built_s{
-	char *name;
-	int (*p)(void);
-} built_s;
-
-/**
- * struct liststr - singly linked list
- * @num: the number field
- * @str: a string
- * @next: points to the next node
+ * struct fmt - types of data and their function
+ * @ltr: the type of data
+ * @type: pointer to function for type
  */
-typedef struct liststr
+typedef struct fmt
 {
-	int num;
-	char *str;
-	struct liststr *next;
-} list_t;
+	char *ltr;
+	int (*type)();
+} fmt;
 
-/**
- *struct passinfo - contains pseudo-arguements to pass into a function,
- *		allowing uniform prototype for function pointer struct
- *@arg: a string generated from getline containing arguements
- *@argv: an array of strings generated from arg
- *@path: a string path for the current command
- *@argc: the argument count
- *@line_count: the error count
- *@err_num: the error code for exit()s
- *@linecount_flag: if on count this line of input
- *@fname: the program filename
- *@env: linked list local copy of environ
- *@environ: custom modified copy of environ from LL env
- *@history: the history node
- *@alias: the alias node
- *@env_changed: on if environ was changed
- *@status: the return status of the last exec'd command
- *@cmd_buf: address of pointer to cmd_buf, on if chaining
- *@cmd_buf_type: CMD_type ||, &&, ;
- *@readfd: the fd from which to read line input
- *@histcount: the history line number count
- */
-typedef struct passinfo
-{
-	char *arg;
-	char **argv;
-	char *path;
-	int argc;
-	unsigned int line_count;
-	int err_num;
-	int linecount_flag;
-	char *fname;
-	list_t *env;
-	list_t *history;
-	list_t *alias;
-	char **environ;
-	int env_changed;
-	int status;
+/* FUNCTIONS */
 
-	char **cmd_buf; /* pointer to cmd ; chain buffer, for memory mangement */
-	int cmd_buf_type; /* CMD_type ||, &&, ; */
-	int readfd;
-	int histcount;
-} info_t;
+void _puts(char *str);
 
+char *_strdup(char *str);
 
-typedef struct builtin_d
-{
-	char *built;
-	void (*f)(char *);
-} builtin_t;
-
-char *_getline(FILE *fp);
-void prompt(int fd, struct stat buf);
-char **tokenizer(char *str);
-char **token_interface(char *, const char *, int);
-char **tokenize(int, char *, const char *);
-int find_path(char *);
-int _putfd(char c, int fd);
-int _putsfd(char *str, int fd);
-int str_len(char *);
-void _eputs(char *);
-void exit_b(char *line);
 char *_strcat(char *dest, char *src);
-char **list_to_strings(list_t *head);
-void double_free(char **);
-void single_free(int, ...);
-char *_getenv(info_t *, const char *);
-int count_token(char *, const char *);
-char *path_finder(char *command);
-char **tokenize_path(int index, char *str);
-char *build_path(char *directory, char *command);
-char *search_directories(char **path_tokens, char *command);
-int shell_env(void);
-int shell_exit(void);
-int builtin_execute(char **tokens);
-int shell_num_builtins(built_s builtin[]);
-char *_env(const char *name);
-char **copy_env(char **environ_copy, unsigned int environ_length);
-char *_which(char *command, char *fullpath, char *path);
-int child(char *fullpath, char **tokens);
-void errors(int error);
-size_t list_len(const list_t *h);
-void free_all(char **tokens, char *path, char *line, char *fullpath, int flag);
-void free_dp(char **array, unsigned int length);
 
+int _strcmp(char *s1, char *s2);
 
-/* builtin functions */
-void cd_b(char *);
-void (*check_built_ins(char *str))(char *str);
-int built_in(char **command, char *line);
-/*Helper functions (string tools)*/
-int _strcmp(char *, char *);
-int _strlen(const char *);
-char *_strdup(const char *);
-char *starts_with(const char *, const char *);
-void print_str(char *, int);
-int print_number(int);
-int _write_char(char);
-char *_memset(char *, char, unsigned int);
-char **get_environ(info_t *);
-list_t *add_node(list_t **, const char *, int);
-list_t *add_node_end(list_t **, const char *, int);
-size_t print_list_str(const list_t *);
-int delete_node_at_index(list_t **, unsigned int);
-int _unsetenv(info_t *, char *);
-int _setenv(info_t *, char *, char *);
-char *get_history_file(info_t *info);
-int write_history(info_t *info);
-int read_history(info_t *info);
-int build_history_list(info_t *info, char *buf, int linecount);
-int renumber_history(info_t *info);
-int _strncmp(char *name, char *var, unsigned int len);
-char *_strcpy(char *dest, char *src);
+int parse(char *line, int num_tokens, char *argv[], int failcount);
 
-#endif
+int exe(char *line, char **ar, char *nln, char **arry, char **argv, int flcnt);
+
+void myfree(char *line, char **ar, char *newline, char **array);
+
+int _strncmp(char *s1, char *s2, int len);
+
+int checkbltin(char *line, char **ar, char *newline, char **array);
+
+int numcount(char *line);
+
+int searchpath(char *p, char **tokens);
+
+int findonpath(char **tokens);
+
+void executepath(char *p, char **tokens);
+
+int _printf(const char *format, ...);
+
+int chkfmt(va_list *args, const char *format, int i);
+
+int prchr(va_list *args);
+
+int prstr(va_list *args);
+
+int prdgt(va_list *args);
+
+void getdigits(int n);
+
+int _putchar(char c);
+
+void changedir(char **ar);
+
+#endif /* HOLBERTON_H */
--- path.c
+++ path.c
@@ -1,177 +1,133 @@
 #include "main.h"
-
 /**
- * path_finder - Acts as an interface for functions that will be able to
- * find the full path of a program.
- * @command: Represents a command. For example ls, echo, pwd, etc.
- * Return: Upon sucess a string with the full path of the program.
- * for example /bin/ls, /bin/echo, etc. Otherwise returns NULL.
+ * _strcpy - copies str from src to dest
+ * @src: source string
+ * @dest: destination string
+ * Return: pointer to destination string
  */
-char *path_finder(char *command)
-{
-	char *str = "PATH";
-	char *constructed;
-	char **path_tokens;
-	int index;
-	char *directory;
-
-	index = find_path(str);
-	path_tokens = tokenize_path(index, str);
-	if (path_tokens == NULL)
-		return (NULL);
-
-	directory = search_directories(path_tokens, command);
-	if (directory == NULL)
-	{
-		double_free(path_tokens);
-		return (NULL);
-	}
-
-	constructed = build_path(directory, command);
-	if (constructed == NULL)
-	{
-		double_free(path_tokens);
-		return (NULL);
-	}
-
-	double_free(path_tokens);
-
-	return (constructed);
-}
-/**
- * find_path - Finds the index of an environmental variable.
- * @str: Environmental variable that needs to be found.
- * Return: Upon success returns the index of the environmental variable.
- * otherwise returns -1.
- */
-int find_path(char *str)
+char *_strcpy(char *dest, char *src)
 {
 	int i;
-	int len;
-	int j;
+	int len = 0;
 
-	len = _strlen(str);
+	while (*(src + len) != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i <= len; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
+}
+/**
+ * executepath - function to access PATH
+ * @p: directory in PATH to access
+ * @tokens: array of tokens to check
+ */
+void executepath(char *p, char **tokens)
+{
+	int status, len, len2;
+	pid_t child;
+	char *newp = NULL;
+
+	for (len = 0; *(p + len) != '\0'; len++)
+		;
+	for (len2 = 0; tokens[0][len2] != '\0'; len2++)
+		;
+	newp = malloc(sizeof(char) * (len + len2 + 2));
+	_strcpy(newp, p);
+	_strcat(newp, "/"); /* concatenates the token onto its dir */
+	_strcat(newp, tokens[0]);
+	newp[(len + len2 + 1)] = '\0';
+
+	child = fork(); /* forks a child */
+
+	if (child == 0)
+	{
+		if (access(newp, X_OK) == 0) /* checks if we have execute permission */
+		{
+			execve(newp, tokens, environ);
+		}
+	}
+	else
+	{
+		while (waitpid(-1, &status, 0) != child) /* waits for child */
+			;
+	}
+	if (status == 0)
+		errno = 0;
+	free(newp);
+}
+
+/**
+ * searchpath - function to search PATH
+ * @p: part of PATH to search
+ * @tokens: array of tokens
+ * Return: 0 for success
+ */
+
+int searchpath(char *p, char **tokens)
+{
+	struct dirent *de;
+	int reached = 0, onpath = -1;
+
+	p = strtok(p, ":"); /* moves to first path */
+	while (p != NULL && reached != 1)
+	{
+		DIR *dr = opendir(p); /* opens current directory pointer */
+
+		if (dr == NULL)
+		{
+			/* not found */
+			return (0);
+		}
+		while ((de = readdir(dr)) != NULL)
+			/* loops through reading directory contents */
+		{
+			if (_strcmp(de->d_name, tokens[0]) == 0)
+			{           /* compares file name to token */
+				executepath(p, tokens);
+				/* if exists, attempt to execute */
+				onpath = 0;
+				/* breaks loops and sets it on path  */
+				reached = 1;
+				break;
+			}
+		}
+		closedir(dr); /* close the open directory */
+		if (reached == 0)
+			p = strtok(NULL, ":");
+		/* continue to look through dir until something is/isn't found*/
+	}
+	return (onpath);
+}
+
+/**
+ * findonpath - find PATH in environ
+ * @tokens: token to pass along to PATH
+ * Return: 0 for success
+ */
+
+int findonpath(char **tokens)
+{
+	int i = 0, onpath;
+	char *p = NULL;
+	char *path = NULL;
+
 	for (i = 0; environ[i] != NULL; i++)
 	{
-		for (j = 0; j < len; j++)
+		if (_strncmp("PATH=", environ[i], 5) == 0)
+			/* loops through environ to get path */
 		{
-			if (environ[i][j] != str[j])
-				break;
-		}
-		if (j == len && environ[i][j] == '=')
-			return (i);
-	}
-	return (-1);
-}
-
-/**
- * tokenize_path - Separates a string representing paths as an array
- * of strings contining the path directories.
- * @index: Index of the path in the environment variables.
- * @str: string to separate and tokenize.
- * Return: Upon success a NULL terminated array of pointer to strings.
- * Otherwise returns NULL. Note!: Do not forget to free alocated
- * memory on receiving function or when possible.
- */
-char **tokenize_path(int index, char *str)
-{
-	char *env_var;
-	int token_count;
-	const char *delim = ":\n";
-	char **path_tokens;
-	int len;
-
-	len = _strlen(str);
-	token_count = 0;
-	/*Moving the pointer len of str plus = sign*/
-	env_var = environ[index] + (len + 1);
-	path_tokens = token_interface(env_var, delim, token_count);
-	if (path_tokens == NULL)
-		return (NULL);
-
-	return (path_tokens);
-}
-
-/**
- * search_directories - Looks through directories stored in path_tokens for a
- * specific file. aka commmand.
- * @path_tokens: A pointer to an array of strings representing the different
- * paths contained in the PATH environmental varible.
- * @command: Represents a command. For example ls, echo, pwd, /bin/ls etc.
- * Return: Upon success a string with the upper most directory containing
- * the command file. Otherwise returns NULL.
- */
-char *search_directories(char **path_tokens, char *command)
-{
-	int i, s;
-	char *cwd;
-	char *buf;
-	size_t size;
-	struct stat stat_buf;
-
-	buf = NULL;
-	size = 0;
-	cwd = getcwd(buf, size);
-	if (cwd == NULL)
-		return (NULL);
-	if (command[0] == '/')
-		command = command + 1;
-	for (i = 0; path_tokens[i] != NULL; i++)
-	{
-		s = chdir(path_tokens[i]);
-		if (s == -1)
-		{
-			perror("ERROR!");
-			return (NULL);
-		}
-		s = stat(command, &stat_buf);
-		if (s == 0)
-		{
-			chdir(cwd);
-			free(cwd);
-			return (path_tokens[i]);
+			path = _strdup(environ[i]);
+			strtok(path, "=");
+			p = strtok(NULL, "="); /* singles out the actual paths */
+			onpath = searchpath(p, tokens); /* search em' */
+			break;
 		}
 	}
-	chdir(cwd);
-	free(cwd);
-	return (NULL);
-}
-
-/**
- * build_path - Combines two strings one representing the path directory and
- * one representing the command file.
- * @directory: Represents a directory in the path.
- * @command: Represents a file in a directory of the path.
- * Return: Upon success a string representing the full path of a command.
- * Otherwise NULL.
- */
-char *build_path(char *directory, char *command)
-{
-	int i, j;
-	int dir_len;
-	int command_len;
-	int len;
-	char *built;
-
-	if (directory == NULL || command == NULL)
-		return (NULL);
-	dir_len = _strlen(directory) + 1;
-	command_len = _strlen(command) + 1;
-	len = dir_len + command_len;
-
-	built = malloc(sizeof(char) * len);
-	if (built == NULL)
-		return (NULL);
-
-	for (i = 0; i < len; i++)
-	{
-		for (j = 0; directory[j] != '\0'; j++, i++)
-			built[i] = directory[j];
-		built[i] = '/';
-		i++;
-		for (j = 0; command[j] != '\0'; j++, i++)
-			built[i] = command[j];
-	}
-	built[--i] = '\0';
-	return (built);
+	free(path);
+	return (onpath); /* return whether or not its on path */
 }
